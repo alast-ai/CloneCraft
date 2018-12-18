bf6160ee0f81123e898d38f3d67ba620deb0978b
Realanthowo <realanthowo@gmail.com>
2018-12-17 19:18:38 -0500

    HelpMe3

--- src/cc/antho/clonecraft/core/ClassRegister.java
+++ src/cc/antho/clonecraft/core/ClassRegister.java
@@ -3,12 +3,17 @@ package cc.antho.clonecraft.core;
 import com.esotericsoftware.kryo.Kryo;
 
 import cc.antho.clonecraft.client.world.BlockType;
+import cc.antho.clonecraft.common.packet.BlockUpdatePacket;
+import cc.antho.clonecraft.common.packet.Packet;
 
 public class ClassRegister {
 
 	public static void register(final Kryo kryo) {
 
 		kryo.register(BlockType.class);
+
+		kryo.register(Packet.class);
+		kryo.register(BlockUpdatePacket.class);
 
 	}
 
