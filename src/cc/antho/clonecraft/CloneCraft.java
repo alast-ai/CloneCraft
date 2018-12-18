4fd55d41361d035025e156f495db94b582268c35
Aaron Lee Marais <aaronleem@gmail.com>
2018-12-18 14:18:15 +0200

    Normalize spaces, change variable names

--- src/cc/antho/clonecraft/CloneCraft.java
+++ src/cc/antho/clonecraft/CloneCraft.java
@@ -28,103 +28,98 @@ public final class CloneCraft {
 
 		Util.setLookAndFeel();
 
-	    JFrame f = new JFrame("A JFrame");
-	    f.setSize(200, 170);
-	    f.setLocation(300, 200);
-	    JTabbedPane tabbedPane = new JTabbedPane();
-	    
-	    GridBagConstraints constraints = new GridBagConstraints();
-	    constraints.anchor = GridBagConstraints.WEST;
-	    constraints.insets = new Insets(5, 5, 5, 5);
-	    
-	    {
-	        JPanel panel = new JPanel(new GridBagLayout());
-	        
-		    final JLabel lblAddress = new JLabel("Host Address");
-		    constraints.gridx = 0;
-	        constraints.gridy = 0;      
-		    panel.add(lblAddress, constraints);
-	        
-		    final JTextField txtAddress = new JTextField(ConnectionDefaults.ADDRESS, 10);
-		    constraints.gridx = 1;
-	        constraints.gridy = 0;      
-		    panel.add(txtAddress, constraints);
-	        
-		    final JLabel lblPort = new JLabel("Host Port");
-		    constraints.gridx = 0;
-	        constraints.gridy = 1;      
-		    panel.add(lblPort, constraints);
-	        
-		    final JTextField txtPort = new JTextField(ConnectionDefaults.TCP_PORT+"", 10);
-		    constraints.gridx = 1;
-	        constraints.gridy = 1;      
-		    panel.add(txtPort, constraints);
-		    
-		    final JButton senpai = new JButton("Connect");
-		    constraints.anchor = GridBagConstraints.EAST;
-		    constraints.gridx = 1;
-	        constraints.gridy = 2;      
-		    panel.add(senpai, constraints);
-		    senpai.addActionListener(new ActionListener() {
-		        @Override
-		        public void actionPerformed(ActionEvent e) {
-		        	CloneCraftClient.main(txtAddress.getText(), Integer.parseInt(txtPort.getText()));
-		        }
-		    });
-		    
-	        tabbedPane.addTab("Client", panel);
-	    }
-	    
-	    {
-	        JPanel panel = new JPanel(new GridBagLayout());
-		    
-		    final JButton senpaiStart = new JButton("Start");
-		    final JButton senpaiStop = new JButton("Stop");
-		    senpaiStop.setEnabled(false);
-		    senpaiStart.setEnabled(true);
-		    
-		    constraints.anchor = GridBagConstraints.EAST;
-		    constraints.gridx = 0;
-	        constraints.gridy = 0;      
-		    panel.add(senpaiStart, constraints);
-		    senpaiStart.addActionListener(new ActionListener() {
+		JFrame f = new JFrame("CloneCraft");
+		f.setSize(200, 170);
+		f.setLocation(300, 200);
+		JTabbedPane tabbedPane = new JTabbedPane();
+		
+		GridBagConstraints constraints = new GridBagConstraints();
+		constraints.anchor = GridBagConstraints.WEST;
+		constraints.insets = new Insets(5, 5, 5, 5);
+		
+		{
+			JPanel panel = new JPanel(new GridBagLayout());
+			tabbedPane.addTab("Client", panel);
+			
+			final JLabel lblAddress = new JLabel("Host Address");
+			constraints.gridx = 0;
+			constraints.gridy = 0;
+			panel.add(lblAddress, constraints);
+			
+			final JTextField txtAddress = new JTextField(ConnectionDefaults.ADDRESS, 10);
+			constraints.gridx = 1;
+			constraints.gridy = 0;
+			panel.add(txtAddress, constraints);
+			
+			final JLabel lblPort = new JLabel("Host Port");
+			constraints.gridx = 0;
+			constraints.gridy = 1;
+			panel.add(lblPort, constraints);
+			
+			final JTextField txtPort = new JTextField(ConnectionDefaults.TCP_PORT+"", 10);
+			constraints.gridx = 1;
+			constraints.gridy = 1;
+			panel.add(txtPort, constraints);
+			
+			final JButton btnConnect = new JButton("Connect");
+			constraints.anchor = GridBagConstraints.EAST;
+			constraints.gridx = 1;
+			constraints.gridy = 2;
+			panel.add(btnConnect, constraints);
+			btnConnect.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					CloneCraftClient.main(txtAddress.getText(), Integer.parseInt(txtPort.getText()));
+				}
+			});
+		}
+		
+		{
+			JPanel panel = new JPanel(new GridBagLayout());
+			tabbedPane.addTab("Server", panel);
+			
+			final JButton serverStart = new JButton("Start");
+			final JButton serverStop = new JButton("Stop");
+			serverStop.setEnabled(false);
+			serverStart.setEnabled(true);
+			
+			constraints.anchor = GridBagConstraints.EAST;
+			constraints.gridx = 0;
+			constraints.gridy = 0;
+			panel.add(serverStart, constraints);
+			serverStart.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					if(CloneCraftServer.instance != null && CloneCraftServer.instance.started) return;
+					if(CloneCraftServer.instance == null) CloneCraftServer.main();
+					
+					serverStop.setEnabled(true);
+					serverStart.setEnabled(false);
+				}
+			});
+			
+			constraints.anchor = GridBagConstraints.EAST;
+			constraints.gridx = 1;
+			constraints.gridy = 0;
+			panel.add(serverStop, constraints);
+			serverStop.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					CloneCraftServer.instance.getServer().stop();
+					serverStop.setEnabled(false);
+					synchronized(CloneCraftServer.instance.getServer().getUpdateThread()) {
+						System.out.println("Server Stopped");
+						CloneCraftServer.instance.started = false;
+						serverStart.setEnabled(true);
+					}
 	
-		        @Override
-		        public void actionPerformed(ActionEvent e) {
-	        		if(CloneCraftServer.instance != null && CloneCraftServer.instance.started) return;
-		        	
-				    if(CloneCraftServer.instance == null) CloneCraftServer.main();
-				    
-				    senpaiStop.setEnabled(true);
-				    senpaiStart.setEnabled(false);
-	
-		        }
-		    });
-		    
-		    constraints.anchor = GridBagConstraints.EAST;
-		    constraints.gridx = 1;
-	        constraints.gridy = 0;      
-		    panel.add(senpaiStop, constraints);
-		    senpaiStop.addActionListener(new ActionListener() {
-		        @Override
-		        public void actionPerformed(ActionEvent e) {
-		        	CloneCraftServer.instance.getServer().stop();
-				    senpaiStop.setEnabled(false);
-		        	synchronized(CloneCraftServer.instance.getServer().getUpdateThread()) {
-			        	System.out.println("Server Stopped");
-			        	CloneCraftServer.instance.started = false;
-					    senpaiStart.setEnabled(true);
-		        	}
-	
-		        }
-		    });
-		    
-	        tabbedPane.addTab("Server", panel);
-	    }
+				}
+			});
+		}
 
-    	f.add(tabbedPane);
-	    f.setVisible(true);
-	    f.setResizable(false);
+		f.add(tabbedPane);
+		f.setVisible(true);
+		f.setResizable(false);
 
 	}
 
